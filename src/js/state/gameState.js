64142a50c7fa6e485bfe011a9d01f73b5ed52f0d
Dwyght <156776807+Dwyght@users.noreply.github.com>
2026-08-23 01:53:37 +0800

    refactor: remove no-op getter/setter boilerplate from GameState

--- src/js/state/gameState.js
+++ src/js/state/gameState.js
@@ -4,102 +4,6 @@ class GameState {
   }
 
   // =========================
-  // GAME CODE
-  // =========================
-
-  get gameCode() {
-    return this._gameCode;
-  }
-
-  set gameCode(value) {
-    this._gameCode = value;
-  }
-
-  // =========================
-  // MY TILE
-  // =========================
-
-  get myTile() {
-    return this._myTile;
-  }
-
-  set myTile(value) {
-    this._myTile = value;
-  }
-
-  // =========================
-  // MY NAME
-  // =========================
-
-  get myName() {
-    return this._myName;
-  }
-
-  set myName(value) {
-    this._myName = value;
-  }
-
-  // =========================
-  // MY SUSHI
-  // =========================
-
-  get mySushi() {
-    return this._mySushi;
-  }
-
-  set mySushi(value) {
-    this._mySushi = value;
-  }
-
-  // =========================
-  // GAME STARTED
-  // =========================
-
-  get gameStarted() {
-    return this._gameStarted;
-  }
-
-  set gameStarted(value) {
-    this._gameStarted = value;
-  }
-
-  // =========================
-  // GAME OVER
-  // =========================
-
-  get gameOver() {
-    return this._gameOver;
-  }
-
-  set gameOver(value) {
-    this._gameOver = value;
-  }
-
-  // =========================
-  // SPECTATOR
-  // =========================
-
-  get isSpectator() {
-    return this._isSpectator;
-  }
-
-  set isSpectator(value) {
-    this._isSpectator = value;
-  }
-
-  // =========================
-  // SCORES
-  // =========================
-
-  get scores() {
-    return this._scores;
-  }
-
-  set scores(value) {
-    this._scores = value;
-  }
-
-  // =========================
   // SET SESSION
   // =========================
 
@@ -111,14 +15,14 @@ class GameState {
     gameStarted = false,
     isSpectator = false,
   }) {
-    this._gameCode = gameCode;
-    this._myTile = myTile;
-    this._myName = myName;
-    this._mySushi = mySushi;
-    this._gameStarted = gameStarted;
-    this._gameOver = false;
-    this._isSpectator = isSpectator;
-    this._scores = {
+    this.gameCode = gameCode;
+    this.myTile = myTile;
+    this.myName = myName;
+    this.mySushi = mySushi;
+    this.gameStarted = gameStarted;
+    this.gameOver = false;
+    this.isSpectator = isSpectator;
+    this.scores = {
       X: 0,
       O: 0,
     };
@@ -129,15 +33,15 @@ class GameState {
   // =========================
 
   reset() {
-    this._gameCode = null;
-    this._myTile = null;
-    this._myName = null;
-    this._mySushi = null;
+    this.gameCode = null;
+    this.myTile = null;
+    this.myName = null;
+    this.mySushi = null;
 
-    this._gameStarted = false;
-    this._gameOver = false;
-    this._isSpectator = false;
-    this._scores = {
+    this.gameStarted = false;
+    this.gameOver = false;
+    this.isSpectator = false;
+    this.scores = {
       X: 0,
       O: 0,
     };
